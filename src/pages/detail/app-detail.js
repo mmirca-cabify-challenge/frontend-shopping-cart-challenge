777e91dc414f28932d98b1ce59bbfde7072395b6
mmirca <marius.mirca@gmail.com>
2020-04-05 19:37:39 +0200

    feat(*): mockup detail page

--- src/pages/detail/app-detail.js
+++ src/pages/detail/app-detail.js
@@ -5,7 +5,28 @@ export class AppDetail extends LitElement {
   render() {
     return html`
       <link href="./assets/css/main.css" rel="stylesheet" />
-      <h1>App detail</h1>
+      <main class="App">
+        <div
+          class="products"
+          style="background-image: url(./assets/img/mug-xl.jpg)"
+        ></div>
+        <div class="summary summary--detail">
+          <div>
+            <div class="summary__close-wrapper">
+              <button class="summary__close">&times;</button>
+            </div>
+            <h1 class="border-bottom-title">
+              <span>Shopping cart</span>
+              <span>20€</span>
+            </h1>
+            <p class="summary__main-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales semper elit sit amet interdum. Praesent volutpat sed elit vel consectetur. Nulla tempus tincidunt ex, sit amet semper ipsum imperdiet varius. In rutrum aliquam nisl, sagittis faucibus felis bibendum id.
+            </p>
+            <p class="summary__code">Product code X7R2OPX</p>
+          </div>
+          <button class="summary__submit" type="submit">Add to cart</button>
+        </div>
+      </main>
     `;
   }
 
