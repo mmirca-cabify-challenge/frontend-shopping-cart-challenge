ad910a8abf3b0421aa1211f22772289bb0326851
mmirca <marius.mirca@gmail.com>
2020-04-11 16:50:03 +0200

    feat(rollup,manifest): add configuration to copy manifest.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,26 @@
 import { getRollupConfig } from 'cap-rollup-config';
 
-export default getRollupConfig({
+const buildConfig = {
   outputDir: 'dist'
+};
+
+export default getRollupConfig({
+  outputDir: buildConfig.outputDir,
+  extender: (pluginConfig, configName) => {
+    switch(configName) {
+      case 'copy':
+        return {
+          ...pluginConfig,
+          targets: pluginConfig.targets.concat({
+            src: [
+              './manifest.json'
+            ],
+            dest: buildConfig.outputDir,
+            copyOnce: true
+          })
+        };
+      default:
+        return pluginConfig;
+    }
+  }
 });
